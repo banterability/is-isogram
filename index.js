08c521373a42e9a7cbd39e4498f948b0fff9b74e
Jeff Long <banterability@users.noreply.github.com>
2017-01-30 11:20:32 -0600

    Handle phrases

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(str) {
-  const letters = str.split('');
+  var letters = str.replace(/ /g, '').split('');
   return letters.length === letters.filter(function(letter, index, arr) {
     return arr.indexOf(letter) == index;
   }).length;
--- test/index.js
+++ test/index.js
@@ -3,11 +3,23 @@ var assert = require('assert');
 var isIsogram = require('../');
 
 describe('isIsogram', function() {
-  it('returns true for isogramatic strings', function() {
-    assert.equal(isIsogram('isogram'), true);
+  describe('words', function() {
+    it('returns true for isogramatic words', function() {
+      assert.equal(isIsogram('isogram'), true);
+    });
+
+    it('returns false for non-isogramatic words', function() {
+      assert.equal(isIsogram('wedding'), false);
+    });
   });
 
-  it('returns false for non-isogramatic strings', function() {
-    assert.equal(isIsogram('wedding'), false);
+  describe('phrases', function() {
+    it('returns true for isogramatic phrases', function() {
+      assert.equal(isIsogram('quick on the draw'), true);
+    });
+
+    it('returns false for non-isogramatic phrases', function() {
+      assert.equal(isIsogram('flipped turned upside down'), false);
+    });
   });
 });

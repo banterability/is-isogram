47255f7110b9a525c8aaf648040b571c4ba92335
Jeff Long <banterability@users.noreply.github.com>
2018-05-10 12:25:08 -0500

    Style cleanup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function isIsogram(str) {
-  var letters = str.replace(/ /g, '').split('');
+  var letters = str.replace(/ /g, "").split("");
+
   return letters.length === letters.filter(
     (letter, index, arr) => arr.indexOf(letter) == index
   ).length;

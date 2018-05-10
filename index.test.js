f3e47db6f5d71bc6901adf4c573254e530687d8e
Jeff Long <banterability@users.noreply.github.com>
2018-05-10 12:26:10 -0500

    Switch test runner to Jest

---
+++ index.test.js
@@ -0,0 +1,23 @@
+var isIsogram = require("./index");
+
+describe("isIsogram", () => {
+  describe("words", () => {
+    test("returns true for isogramatic words", () => {
+      expect(isIsogram("sandwich")).toEqual(true);
+    });
+
+    test("returns false for non-isogramatic words", () => {
+      expect(isIsogram("wedding")).toEqual(false);
+    });
+  });
+
+  describe("phrases", () => {
+    test("returns true for isogramatic phrases", () => {
+      expect(isIsogram("quick on the draw")).toEqual(true);
+    });
+
+    test("returns false for non-isogramatic phrases", () => {
+      expect(isIsogram("flipped turned upside down")).toEqual(false);
+    });
+  });
+});
